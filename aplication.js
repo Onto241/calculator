7968d9444ef2b6ec3468655cfd005509a2e4febd
Onto241 <dovidavicius.marius@gmail.com>
2017-06-21 18:24:37 +0100

    Calculator

---
+++ aplication.js
@@ -0,0 +1,29 @@
+var Calculator = (function () {
+    var amount = 0;
+    return {
+        plus: function (number) {
+            amount += number;
+            console.log(amount);
+            return this;
+        },
+        minus: function (number) {
+            amount -= number;
+            console.log(amount);
+            return this;
+        },
+        multiply: function (number) {
+            amount *= number;
+            console.log(amount);
+            return this;
+        },
+        divide: function (number) {
+            amount /= number;
+            console.log(amount);
+            return this;
+        },
+        value: function () {
+            var total = amount;
+            return total;
+        }
+    }
+} ());
